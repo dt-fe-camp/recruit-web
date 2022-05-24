1c4145f4982625a7565f06968bc6d6e1d822bfa6
afcfzf <9301462@qq.com>
2022-05-24 23:47:03 +0800

    build

---
+++ release/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CURRENT_DIR="$(dirname "$(realpath $0)")";
+cd ${CURRENT_DIR}/../
+
+ROOT_DIR=$(pwd);
+printf "%s\n" $ROOT_DIR;
+
+SERVER_BUILDER="${ROOT_DIR}/build-server.sh";
+printf "%s" $SERVER_BUILDER;
+
+# server
+cd "${ROOT_DIR}/server";
+mvn clean package -Dmaven.test.skip=true;
+# sh $SERVER_BUILDER;
